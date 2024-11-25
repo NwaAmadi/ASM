bd8249db526a329358fe83464acaf41e641a67ab
Gospel <chibuisiukegbu@gmail.com>
2024-11-25 14:09:06 +0100

    Fixed Login issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import './CSS/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AdminLogin from './SubComponents/AdminLogin';
 import Home from './Maincomponents/Home';
 import ConferenceHome from './SubComponents/ConferenceHome';
 import Navbar from './SubComponents/Navbar';
 import Footer from './SubComponents/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true); 
+  };
+
   return (
     <Router>
       <Routes>
         {/* Routes with Navbar */}
         <Route path="/" element={<><Navbar /><ConferenceHome /> <Footer /></>} />
-        <Route path="/login" element={<><Navbar /><AdminLogin /></>} />
+        <Route path="/login" element={<><Navbar /><AdminLogin onLoginSuccess={handleLoginSuccess} /></>} />
 
-        {/* Route without Navbar */}
-        <Route path="/home" element={<Home />} />
+        {/* Protected Route - Only accessible if logged in */}
+        <Route 
+          path="/home" 
+          element={isLoggedIn ? <Home /> : <Navigate to="/login" />} 
+        />
       </Routes>
-      
     </Router>
   );
 }
