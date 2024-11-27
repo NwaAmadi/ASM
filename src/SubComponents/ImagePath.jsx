9fbc81e637d06940703e74eea5f0838c1bb54946
Gospel <chibuisiukegbu@gmail.com>
2024-11-27 15:20:14 +0100

    Starting the darkmode

--- src/SubComponents/ImagePath.jsx
+++ src/SubComponents/ImagePath.jsx
@@ -3,16 +3,16 @@ function ImagePath(){
     let num =  Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     let path;
     if (num === 1) {
-        path = '../../Assets/Tagpix.svg';
+        path = '/public/Assets/Tagpix.svg';
     } 
     else if (num === 2) {
-        path = '../../Assets/Tagpix2.svg';
+        path = '/public/Assets/Tagpix2.svg';
     }
     else if (num === 3) {
-        path = '../../Assets/Tagpix3.svg';
+        path = '/public/Assets/Tagpix3.svg';
     }
     else if (num === 4){
-        path = '../../Assets/Tagpix4.svg';
+        path = '/public/Assets/Tagpix4.svg';
     }
     return(path);
 }
--- src/SubComponents/InfoIcon.jsx
+++ src/SubComponents/InfoIcon.jsx
@@ -1,9 +1,8 @@
 import '../CSS/InfoIcon.css';
-import Icon from '/public/Assets/infoIcon.svg';
 function InfoIcon() {
     return(
         <div className="infoIcon">
-            <img src={Icon} alt="Logo"/>
+            <img src='/Assets/infoIcon.svg' alt="Logo"/>
         </div>
     );
 }
