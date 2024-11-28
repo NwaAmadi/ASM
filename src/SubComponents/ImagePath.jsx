d7c66adb99ba44e65a7c9bfa3c704655533b3296
Gospel <chibuisiukegbu@gmail.com>
2024-11-28 16:28:52 +0100

    fixed

--- src/SubComponents/ImagePath.jsx
+++ src/SubComponents/ImagePath.jsx
@@ -1,18 +1,18 @@
-
+import '../../public/Assets'
 function ImagePath(){
     let num =  Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     let path;
     if (num === 1) {
-        path = '/public/Assets/Tagpix.svg';
+        path = '../../public/Assets/Tagpix.svg';
     } 
     else if (num === 2) {
-        path = '/public/Assets/Tagpix2.svg';
+        path = '../../public/Assets/Tagpix2.svg';
     }
     else if (num === 3) {
-        path = '/public/Assets/Tagpix3.svg';
+        path = '../../public/Assets/Tagpix3.svg';
     }
     else if (num === 4){
-        path = '/public/Assets/Tagpix4.svg';
+        path = '../../public/Assets/Tagpix4.svg';
     }
     return(path);
 }
