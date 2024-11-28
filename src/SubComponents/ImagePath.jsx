c3c91ea2f9ae8f021cc86eee2fa819320d643f75
Gospel <chibuisiukegbu@gmail.com>
2024-11-28 17:07:46 +0100

    Fixed

--- src/SubComponents/ImagePath.jsx
+++ src/SubComponents/ImagePath.jsx
@@ -1,23 +1,26 @@
-import Tagpix from '/Assets/Tagpix.svg';
-import Tagpix2 from '/Assets/Tagpix2.svg';
-import Tagpix3 from '/Assets/Tagpix3.svg';
-import Tagpix4 from '/Assets/Tagpix4.svg';
+import Tagpix from '../public/Assets/Tagpix.svg';
+import Tagpix2 from '../public/Assets/Tagpix2.svg';
+import Tagpix3 from '../public/Assets/Tagpix3.svg';
+import Tagpix4 from '../public/Assets/Tagpix4.svg';
 
-function ImagePath(){
-    let num =  Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    let path;
-    if (num === 1) {
-        path = {Tagpix};
-    } 
-    else if (num === 2) {
-        path = {Tagpix2};
-    }
-    else if (num === 3) {
-        path = {Tagpix3};
-    }
-    else if (num === 4){
-        path = {Tagpix4};
-    }
-    return(path);
+function ImagePath() {
+  
+  let num = Math.floor(Math.random() * 4) + 1; 
+  let path;
+
+  
+  if (num === 1) {
+    path = Tagpix;
+  } else if (num === 2) {
+    path = Tagpix2;
+  } else if (num === 3) {
+    path = Tagpix3;
+  } else if (num === 4) {
+    path = Tagpix4;
+  }
+
+  
+  return <img src={path} alt="Random Image" />;
 }
-export default ImagePath
+
+export default ImagePath;
