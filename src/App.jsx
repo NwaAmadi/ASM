bd2f838b234facc98a2dcd93f14693bd763f1904
Gospel <chibuisiukegbu@gmail.com>
2025-01-25 20:41:16 +0100

    Fixed double footer on admin page

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
           element={isLoggedIn ? (
             <>
               <Home />
-              <Footer />
+              
             </>
           ) : (
             <Navigate to="/" />
